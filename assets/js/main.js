0b04fe5cfa83c58bc53a0176c11a35e196c3d1e2
Renato Moreira <131251056+RenatoMor@users.noreply.github.com>
2025-03-07 16:11:13 -0300

    versão 1.5.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ function updateSoftSkills(profileData) {
 
 function updateHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}" info="${skill.info}"></li>`).join('')
 }
 
 function updateLanguages(profileData) {
